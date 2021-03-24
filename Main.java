2c983ac3c90c9d40107c7021c15aa451ed2ae59b
Jahid Hasan <vdjsovaj@gmail.com>
2021-03-24 18:02:49 +0600

    worker print

--- Main.java
+++ Main.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import javax.sound.midi.VoiceStatus;
 
 public class Main {
+
+    private static final String FORMAL = "Formal";
+    private static Void printAllWorkers;
 
     public static void main(String[] args) {
 
         SuperShop raptor = new SuperShop("Raptor", "khagan, Ashulia, Savar", "01773393608");
 
-        Worker w = new Worker("Jahid Hasan", 20, "Male", 201, "F", "010000000", 200.30, "day", 8);
-        Worker w1 = new Worker("Hasib", 20, "Male", 201, "Fo", "0100000001", 200.30, "day", 8);
-        Worker w2 = new Worker("nirob", 20, "Male", 201, "For", "0100000002", 200.30, "day", 8);
-        Worker w3 = new Worker("payel", 20, "Female", 201, "Form", "0100000003", 200.30, "day", 8);
-        Worker w4 = new Worker("likhn", 20, "Male", 201, "Forma", "0100000004", 200.30, "day", 8);
-        Worker w5 = new Worker("badol", 20, "Male", 201, "Formal", "0100000005", 200.30, "day", 8);
+        Worker w = new Worker("Jahid Hasan", 20, "Male", 201, FORMAL, "010000000", 200.30, "day", 8);
+        Worker w1 = new Worker("Hasib", 20, "Male", 201, FORMAL, "0100000001", 200.30, "day", 8);
+        Worker w2 = new Worker("nirob", 20, "Male", 201, FORMAL, "0100000002", 200.30, "day", 8);
+        Worker w3 = new Worker("payel", 20, "Female", 201, FORMAL, "0100000003", 200.30, "day", 8);
+        Worker w4 = new Worker("likhn", 20, "Male", 201, FORMAL, "0100000004", 200.30, "day", 8);
+        Worker w5 = new Worker("badol", 20, "Male", 201, FORMAL, "0100000005", 200.30, "day", 8);
 
         raptor.addWorkers(w);
         raptor.addWorkers(w1);
@@ -21,13 +27,38 @@ public class Main {
         raptor.addWorkers(w4);
         raptor.addWorkers(w5);
 
-        try {
-            for (int i = 0; i < raptor.getWorkers().size(); i++) {
-                System.out.println(raptor.getWorkers().get(i).getName());
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("\nSelect your Option: ");
+            System.out.println("-----------------------");
+            System.out.println("1. Add");
+            System.out.println("2. Print");
+            System.out.println("3. Search");
+            System.out.println("4. Delete");
+            System.out.println("5. Modify");
+            System.out.println("6. Exit");
+            System.out.print("\nEnter Option: ");
+
+            int option = input.nextInt();
+            if (option == 6) {
+                input.close();
+                break;
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+
+            switch (option) {
+            case 2:
+                raptor.printAllWorkers();
+                break;
+
+            default:
+                System.out.println("You need to choose Right option");
+                break;
+            }
+
         }
 
     }
+
 }
--- SuperShop.java
+++ SuperShop.java
@@ -34,4 +34,27 @@ public class SuperShop {
         workers.add(worker);
     }
 
+    public void printAllWorkers() {
+
+        try {
+
+            for (int i = 0; i < getWorkers().size(); i++) {
+                System.out.println("\n-------------------------------------");
+                System.out.println("            Worker Details             ");
+                System.out.println("-------------------------------------\n");
+                System.out.println("Name: " + getWorkers().get(i).getName());
+                System.out.println("ID: " + getWorkers().get(i).getId());
+                System.out.println("Age: " + getWorkers().get(i).getAge());
+                System.out.println("Gender: " + getWorkers().get(i).getGender());
+                System.out.println("Phone Number" + getWorkers().get(i).getPhoneNumber());
+                System.out.println("Salary: " + getWorkers().get(i).getSalary());
+                System.out.println("Work Hour: " + getWorkers().get(i).getWorkHour());
+                System.out.println("Shift: " + getWorkers().get(i).getDutyShift());
+                System.out.println("DressCode: " + getWorkers().get(i).getDressCode());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
